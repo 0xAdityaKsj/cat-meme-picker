9c4973343d383b3bb71bdd366877afcb94006ccb
Aditya <aditya.kantipudi@protonmail.com>
2023-02-08 20:02:15 +0530

    experimenting with the close modal box outside feature by clicking outside the modal

--- index.js
+++ index.js
@@ -6,28 +6,39 @@ const gifsOnlyOption = document.getElementById('gifs-only-option')
 const memeModalInner = document.getElementById('meme-modal-inner')
 const memeModal = document.getElementById('meme-modal')
 const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+const bodyEl = document.getElementsByTagName('body')
+
+
+bodyEl.addEventListener('click', function (e) {
+    if (!e.target.id === 'meme-modal' || !e.target.id === 'main') {
+        closeModal
+    }
+})
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 memeModalCloseBtn.addEventListener('click', closeModal)
 
+
+
+
 getImageBtn.addEventListener('click', renderCat)
 
-function highlightCheckedOption(e){
+function highlightCheckedOption(e) {
     const radios = document.getElementsByClassName('radio')
-    for (let radio of radios){
+    for (let radio of radios) {
         radio.classList.remove('highlight')
     }
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
-function closeModal(){
+function closeModal() {
     memeModal.style.display = 'none'
 }
 
-function renderCat(){
+function renderCat() {
     const catObject = getSingleCatObject()
-    memeModalInner.innerHTML =  `
+    memeModalInner.innerHTML = `
         <img 
         class="cat-img" 
         src="./images/${catObject.image}"
@@ -37,41 +48,41 @@ function renderCat(){
     memeModal.style.display = 'flex'
 }
 
-function getSingleCatObject(){
+function getSingleCatObject() {
     const catsArray = getMatchingCatsArray()
-    
-    if(catsArray.length === 1){
+
+    if (catsArray.length === 1) {
         return catsArray[0]
     }
-    else{
+    else {
         const randomNumber = Math.floor(Math.random() * catsArray.length)
         return catsArray[randomNumber]
     }
 }
 
-function getMatchingCatsArray(){     
-    if(document.querySelector('input[type="radio"]:checked')){
+function getMatchingCatsArray() {
+    if (document.querySelector('input[type="radio"]:checked')) {
         const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
         const isGif = gifsOnlyOption.checked
-        
-        const matchingCatsArray = catsData.filter(function(cat){
-            
-            if(isGif){
+
+        const matchingCatsArray = catsData.filter(function (cat) {
+
+            if (isGif) {
                 return cat.emotionTags.includes(selectedEmotion) && cat.isGif
             }
-            else{
+            else {
                 return cat.emotionTags.includes(selectedEmotion)
-            }            
+            }
         })
-        return matchingCatsArray 
-    }  
+        return matchingCatsArray
+    }
 }
 
-function getEmotionsArray(cats){
-    const emotionsArray = []    
-    for (let cat of cats){
-        for (let emotion of cat.emotionTags){
-            if (!emotionsArray.includes(emotion)){
+function getEmotionsArray(cats) {
+    const emotionsArray = []
+    for (let cat of cats) {
+        for (let emotion of cat.emotionTags) {
+            if (!emotionsArray.includes(emotion)) {
                 emotionsArray.push(emotion)
             }
         }
@@ -79,11 +90,11 @@ function getEmotionsArray(cats){
     return emotionsArray
 }
 
-function renderEmotionsRadios(cats){
-        
+function renderEmotionsRadios(cats) {
+
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions){
+    for (let emotion of emotions) {
         radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
